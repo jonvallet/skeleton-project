8e712424c47a2537f7a2626be477cbd9ad52e25a
jvallet <jon.weadon@proxama.com>
2014-11-27 09:15:14 +0000

    Added an example class and test.

---
+++ src/main/java/Example.java
@@ -0,0 +1,7 @@
+public class Example {
+
+    public static void main (String [] argsv){
+        System.out.println("This is an example class.");
+    }
+
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest(){
+        Assert.fail();
+    }
+}
